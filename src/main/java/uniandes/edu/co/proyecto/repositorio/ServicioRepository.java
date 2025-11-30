3599a1834593e8026d3caa68033d2b9d350d231e
Stiff0126-aj <m.londonot@uniandes.edu.co>
2025-11-30 18:43:28 -0500

    RFC's

--- src/main/java/uniandes/edu/co/proyecto/repositorio/ServicioRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/ServicioRepository.java
@@ -19,19 +19,24 @@ public interface ServicioRepository extends MongoRepository<Servicio, String> {
 
     // RFC2: top 20 conductores con más servicios
     @Aggregation(pipeline = {
-        "{ '$match': { 'conductorAsignadoId': { $ne: null } } }",
-        "{ '$group': { '_id': '$conductorAsignadoId', 'totalServicios': { '$sum': 1 } } }",
-        "{ '$sort': { 'totalServicios': -1 } }",
-        "{ '$limit': 20 }"
+    "{ '$match': { 'conductorAsignadoId': { $ne: null } } }",
+    "{ '$group': { '_id': '$conductorAsignadoId', 'totalServicios': { '$sum': 1 } } }",
+    "{ '$lookup': { 'from': 'conductores', 'localField': '_id', 'foreignField': '_id', 'as': 'conductor' } }",
+    "{ '$unwind': '$conductor' }",
+    "{ '$sort': { 'totalServicios': -1 } }",
+    "{ '$limit': 20 }"
     })
     List<ConductorServiciosCount> obtenerTop20Conductores();
 
     // RFC3: estadísticas de uso por ciudad y fechas
     @Aggregation(pipeline = {
-        "{ '$match': { 'puntoPartida.ciudad': ?0, 'horaInicio': { $gte: ?1, $lte: ?2 } } }",
-        "{ '$group': { '_id': '$tipo', 'totalServicios': { '$sum': 1 } } }",
-        "{ '$sort': { 'totalServicios': -1 } }"
+    "{ '$match': { 'puntoPartida.ciudad': ?0, 'horaInicio': { $gte: ?1, $lte: ?2 } } }",
+    "{ '$group': { '_id': '$tipo', 'totalServicios': { '$sum': 1 } } }",
+    "{ '$group': { '_id': null, 'total': { '$sum': '$totalServicios' }, 'detalles': { '$push': { 'tipo': '$_id', 'totalServicios': '$totalServicios' } } } }",
+    "{ '$unwind': '$detalles' }",
+    "{ '$project': { 'tipo': '$detalles.tipo', 'totalServicios': '$detalles.totalServicios', 'porcentaje': { '$multiply': [ { '$divide': ['$detalles.totalServicios', '$total'] }, 100 ] } } }",
+    "{ '$sort': { 'totalServicios': -1 } }"
     })
-    List<TipoServicioStats> obtenerUsoServiciosPorCiudadYFechas(
-            String ciudad, Date fechaInicio, Date fechaFin);
+    List<TipoServicioStats> obtenerUsoServiciosPorCiudadYFechas(String ciudad, Date fechaInicio, Date fechaFin);
+
 }
